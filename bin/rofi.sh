f6b4144493c06fe80458a3b133ef2e0cde6888f0
Twan Driessen <tdriess0@xs4all.net>
2020-04-29 11:25:46 +0200

    Blue and green are less urgent

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -3,6 +3,6 @@
 bg_color=#1C1B19
 border_color=#2D2B28
 text_color=#FCE8C3
-htext_color=#F75341
+htext_color=#5573A3
 
 rofi -show run -lines 5 -eh 1 -width 20 -bw 2 -no-show-match -hide-scrollbar -color-window "$bg_color, $border_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "System San Francisco Display 11"
